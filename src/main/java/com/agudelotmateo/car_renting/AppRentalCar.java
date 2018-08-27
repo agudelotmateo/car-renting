167edadf15ae9030e7752e77d332ff913c0b9499
agudelotmateo <agudelotmateo@gmail.com>
2018-08-26 21:40:38 -0500

    Initial commit

---
+++ src/main/java/com/agudelotmateo/car_renting/AppRentalCar.java
@@ -0,0 +1,11 @@
+package com.agudelotmateo.car_renting;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppRentalCar {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
